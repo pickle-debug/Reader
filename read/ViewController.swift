f5252ba15fdae236d965bf47a5d0e0e802fc87aa
gidon_ho <1941109171@qq.com>
2025-08-20 10:08:57 +0800

    Initial Commit

---
+++ read/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  read
+//
+//  Created by gidon on 2025/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
